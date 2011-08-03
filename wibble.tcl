b6f50adc501a261dfaf50af7a5b424a834bf1a98
Jeremy Cowgar <jeremy@cowgar.com>
2011-08-02 20:10:11 -0400

    Removed example from wibble.tcl source directly

--- wibble.tcl
+++ wibble.tcl
@@ -964,29 +964,5 @@ proc ::wibble::log {message} {
     chan puts stderr $message
 }
 
-# =============================== example code ================================
-
-# Demonstrate Wibble if being run directly.
-if {$argv0 eq [info script]} {
-    # Guess the root directory.
-    set root [file normalize [file dirname [info script]]]
-
-    # Define zone handlers.
-    ::wibble::handle /vars vars
-    ::wibble::handle / dirslash root $root
-    ::wibble::handle / indexfile root $root indexfile index.html
-    ::wibble::handle / static root $root
-    ::wibble::handle / template root $root
-    ::wibble::handle / script root $root
-    ::wibble::handle / dirlist root $root
-    ::wibble::handle / notfound
-
-    # Start a server and enter the event loop.
-    catch {
-        ::wibble::listen 8080
-        vwait forever
-    }
-}
-
 # vim: set sts=4 sw=4 tw=80 et ft=tcl:
 
